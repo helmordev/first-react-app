65f25aa4d1eb89841f93743fbb3b9001cf2b0a83
Poging Programmer <helmorpogi@gmail.com>
2025-07-01 20:58:31 +0800

    test commit
    style: remove the color red of text
    
    testa

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
 const App = () => {
-  return <div className="text-center text-bold text-2xl text-red-700">App</div>;
+  return <div className="text-center text-bold text-2xl">App test of nvim test</div>;
 };
 export default App;
