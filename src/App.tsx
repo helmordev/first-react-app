b141023917bb916a7ca3188a45841ed07724b5ee
Poging Programmer <helmorpogi@gmail.com>
2025-07-01 21:12:15 +0800

    style: remove the color red of text
    testa

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
 const App = () => {
-  return <div className="text-center text-bold text-2xl text-red-700">App test of nvim</div>;
+  return <div className="text-center text-bold text-2xl">App test of nvim</div>;
 };
 export default App;
